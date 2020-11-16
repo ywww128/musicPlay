3a1607170698b523fc65ace9c63b0cfb73690cbe
porphyra3 <1454842862@qq.com>
2020-11-16 19:26:36 +0800

    配置XUI框架

--- app/src/main/java/com/example/musicplayer/activity/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.musicplayer;
+package com.example.musicplayer.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.musicplayer.R;
 
 public class MainActivity extends AppCompatActivity {
 
