5c1aed1f8c495e177da5b3dec7d4706b98fc72c2
porphyra3 <1454842862@qq.com>
2020-11-16 23:47:59 +0800

    mainactivity

--- app/src/main/java/com/example/musicplayer/activity/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/activity/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.musicplayer.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import com.example.musicplayer.R;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
