5ab9cbf952866ebefe086c13220d729b2bc2c0d0
porphyra3 <1454842862@qq.com>
2020-11-20 00:04:47 +0800

    MainActivit和MainActivity2的布局文件及MainActivity代码

--- app/src/main/java/com/example/musicplayer/activity/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/activity/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.musicplayer.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import com.example.musicplayer.R;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.example.musicplayer.R;
+import com.xuexiang.xui.widget.button.ButtonView;
+import com.xuexiang.xui.widget.button.shinebutton.ShineButton;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author czc
+ */
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButtonView button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
