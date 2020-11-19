cabb34d84ff56f98a0a0e2c404c417041aefd2a5
porphyra3 <1454842862@qq.com>
2020-11-20 00:07:49 +0800

    MainActivity.java

--- app/src/main/java/com/example/musicplayer/activity/MainActivity2.java
+++ app/src/main/java/com/example/musicplayer/activity/MainActivity2.java
@@ -1,14 +1,29 @@
-package com.example.musicplayer;
+package com.example.musicplayer.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity2 extends AppCompatActivity {
+import com.example.musicplayer.R;
+import com.example.musicplayer.fragment.CommunityFragment;
 
+public class MainActivity2 extends FragmentActivity {
+    private CommunityFragment communityFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        init();
+    }
+
+    private void init(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        communityFragment = new CommunityFragment();
+        fragmentTransaction.add(R.id.container, communityFragment);
+        fragmentTransaction.commit();
     }
 }
