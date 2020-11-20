f9813ad1296608d6eb6f24041fafe6e5628fb776
porphyra3 <1454842862@qq.com>
2020-11-21 00:18:37 +0800

    设置发布动态界面的按钮监听器

---
+++ app/src/main/java/com/example/musicplayer/fragment/PublishFragment.java
@@ -0,0 +1,62 @@
+package com.example.musicplayer.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.musicplayer.R;
+import com.xuexiang.xui.widget.actionbar.TitleBar;
+import com.xuexiang.xui.widget.edittext.MultiLineEditText;
+
+/**
+ * 发表动态fragment，对应fragment_publish
+ * @author czc
+ */
+public class PublishFragment extends Fragment {
+    private TitleBar titleBar;
+    private MultiLineEditText editText;
+    public PublishFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_publish, container, false);
+        init(view);
+        return view;
+    }
+    private void init(View view){
+        titleBar = view.findViewById(R.id.tb_publish);
+        editText = view.findViewById(R.id.publish_text);
+        //设置标题栏左边按钮监听器，点击返回社区主界面
+        titleBar.setLeftClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, new CommunityFragment(), null)
+                        .addToBackStack(null).commit();
+                Toast.makeText(getActivity(), "点击了返回按钮", Toast.LENGTH_SHORT).show();
+            }
+            //设置标题栏右边按钮监听器,点击发布动态
+        }).addAction(new TitleBar.ImageAction(R.drawable.confirm) {
+            @Override
+            public void performAction(View view) {
+
+                Toast.makeText(getActivity(), "您输入了:"+editText.getContentText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
