08dacf97553d8596c3799b3a4407988e055a5db0
porphyra3 <1454842862@qq.com>
2020-11-30 19:49:30 +0800

    发布动态界面添加用户信息

--- app/src/main/java/com/example/musicplayer/fragment/PublishFragment.java
+++ app/src/main/java/com/example/musicplayer/fragment/PublishFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 
 import com.example.musicplayer.R;
 import com.example.musicplayer.utils.DataUtil;
+import com.xuexiang.xui.utils.SnackbarUtils;
 import com.xuexiang.xui.widget.actionbar.TitleBar;
 import com.xuexiang.xui.widget.edittext.MultiLineEditText;
 
@@ -27,7 +29,10 @@ public class PublishFragment extends Fragment {
 
     private TitleBar titleBar;
     private MultiLineEditText editText;
+
     private String username;
+
+
     public PublishFragment() {
         // Required empty public constructor
     }
@@ -57,38 +62,60 @@ public class PublishFragment extends Fragment {
         init(view);
         return view;
     }
+
+    /**
+     * 初始化控件
+     * @param view 包含控件的视图
+     */
     private void init(View view){
         titleBar = view.findViewById(R.id.tb_publish);
         editText = view.findViewById(R.id.publish_text);
+
         //设置标题栏左边按钮监听器，点击返回社区主界面
         titleBar.setLeftClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, CommunityFragment.newInstance(username), "cf")
-                        .addToBackStack(null).commit();
+                PublishFragment publishFragment = (PublishFragment) getActivity().getSupportFragmentManager().findFragmentByTag("pf");
+                CommunityFragment communityFragment = (CommunityFragment) getActivity().getSupportFragmentManager().findFragmentByTag("cf");
+                getActivity().getSupportFragmentManager().beginTransaction().hide(publishFragment).commit();
+                getActivity().getSupportFragmentManager().beginTransaction().show(communityFragment).commit();
                 Toast.makeText(getActivity(), "点击了返回按钮", Toast.LENGTH_SHORT).show();
             }
             //设置标题栏右边按钮监听器,点击发布动态
         }).addAction(new TitleBar.ImageAction(R.drawable.confirm) {
             @Override
             public void performAction(View view) {
-                writeSituation();
-                Toast.makeText(getActivity(), "您输入了:"+editText.getContentText(), Toast.LENGTH_SHORT).show();
-                PublishFragment publishFragment = (PublishFragment) getActivity().getSupportFragmentManager().findFragmentByTag("pf");
-                CommunityFragment communityFragment = (CommunityFragment) getActivity().getSupportFragmentManager().findFragmentByTag("cf");
-                getActivity().getSupportFragmentManager().beginTransaction().hide(publishFragment).commit();
-                getActivity().getSupportFragmentManager().beginTransaction().show(communityFragment).commit();
-                communityFragment.initData();
+                String info = editText.getContentText();
+                Toast.makeText(getActivity(), "您输入了:"+info, Toast.LENGTH_SHORT).show();
+                //判断输入书否为空,内容为空跳出提示信息
+                if(info.equals("")){
+                    //设置内容在顶部显示
+                    SnackbarUtils.Short(view, "发布动态内容不能为空").gravityFrameLayout(Gravity.TOP)
+                            .messageCenter().warning().show();
+                    Toast.makeText(getActivity(), "发布动态内容不能为空", Toast.LENGTH_SHORT).show();
+                }else{
+                    writeSituation();
+                    PublishFragment publishFragment = (PublishFragment) getActivity().getSupportFragmentManager().findFragmentByTag("pf");
+                    CommunityFragment communityFragment = (CommunityFragment) getActivity().getSupportFragmentManager().findFragmentByTag("cf");
+                    getActivity().getSupportFragmentManager().beginTransaction().hide(publishFragment).commit();
+                    getActivity().getSupportFragmentManager().beginTransaction().show(communityFragment).commit();
+                    communityFragment.initData();
+                }
             }
         });
     }
+
+    /**
+     * 将用户发布的内容写回
+     */
     private void writeSituation(){
         Date now = new Date();
         SimpleDateFormat df = new SimpleDateFormat("MM月dd日");
-        String info = "11 2131230850 "+df.format(now)+" "+username+" "+editText.getContentText();
+        String info = "11,2131230850,"+df.format(now)+","+username+","+editText.getContentText() + ",0";
         Log.i("username",username);
         DataUtil.situation.add(info);
         Log.i("publishFragment",DataUtil.situation.toString());
     }
+
 
 }
