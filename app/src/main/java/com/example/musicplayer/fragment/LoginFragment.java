4cec80a8671a6240e406bb04382050e746be2841
ywww <1285324136@qq.com>
2020-12-24 21:39:04 +0800

    登录连接服务器实现

--- app/src/main/java/com/example/musicplayer/fragment/LoginFragment.java
+++ app/src/main/java/com/example/musicplayer/fragment/LoginFragment.java
@@ -3,6 +3,7 @@ package com.example.musicplayer.fragment;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.musicplayer.R;
 import com.example.musicplayer.activity.MainActivity;
+import com.example.musicplayer.volley.UserLoginCheck;
 import com.xuexiang.xui.widget.button.roundbutton.RoundButton;
 import com.xuexiang.xui.widget.imageview.RadiusImageView;
 
@@ -33,19 +35,25 @@ import java.util.Map;
  */
 public class LoginFragment extends Fragment {
     private View view;
-    private EditText usernameView;
+    private MyAccountFragment myAccountFragment;
+    private EditText idView;
     private EditText passwordView;
     private TextView backView;
     private RoundButton loginButton;
     private TextView toRegisterView;
     private MainActivity mainActivity;
     private FragmentManager fManager;
-    private boolean isFromRegister = false;
+    private String id;
+    private String password;
+    public  LoginFragment(){}
+    public LoginFragment(MyAccountFragment myAccountFragment){
+        this.myAccountFragment = myAccountFragment;
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_login,null);
-        usernameView = view.findViewById(R.id.username_login_text);
+        idView = view.findViewById(R.id.id_login_text);
         passwordView = view.findViewById(R.id.password_login_text);
         backView = view.findViewById(R.id.cancel_login_view);
         loginButton = view.findViewById(R.id.login_button);
@@ -72,51 +80,84 @@ public class LoginFragment extends Fragment {
         toRegisterView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                isFromRegister = true;
+                clearFocus();
                 FragmentTransaction fTransaction = fManager.beginTransaction();
                 fTransaction.hide(LoginFragment.this);
                 fTransaction.add(R.id.content_panel,new RegisterFragment()).addToBackStack(null).commit();
             }
         });
 
-        // 点击登录按钮监听器(暂用，后续需要服务器）
+        // 点击登录按钮监听器进行服务器验证
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = String.valueOf(usernameView.getText());
-                String password = String.valueOf(passwordView.getText());
-                if(username != null && password != null){
-                    SharedPreferences sharedPreferences = mainActivity.getSharedPreferences("user", Context.MODE_PRIVATE);
-                    Map<String,String> map = (Map<String, String>) sharedPreferences.getAll();
-                    // 判断账号是否存在
-                    if(map.containsKey(username)){
-                        // 取出json数据
-                        String value = map.get(username);
-                        try {
-                            JSONObject jsonObject = new JSONObject(value);
-                            if(jsonObject.getString("password").equals(password)){
-                                Toast.makeText(getActivity(),"登录成功",Toast.LENGTH_SHORT).show();
-                                // 修改为已登录状态，此状态只允许一个账号为true
-                                jsonObject.put("isLogin",true);
-                                SharedPreferences.Editor editor = sharedPreferences.edit();
-                                editor.putString(username,String.valueOf(jsonObject));
-                                editor.commit();
-                                // 登录成功返回
-                                fManager.popBackStack();
-                            } else {
-                                Toast.makeText(getActivity(),"密码错误",Toast.LENGTH_SHORT).show();
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    } else {
-                        Toast.makeText(getActivity(),"该账号不存在",Toast.LENGTH_SHORT).show();
-                    }
+                id = String.valueOf(idView.getText());
+                password = String.valueOf(passwordView.getText());
+                // 判断密码和账号是否有为空的情况
+                if(!"".equals(id) && !"".equals(password)){
+                    // 传入服务器进行登录操作
+                    UserLoginCheck userLoginCheck = new UserLoginCheck(mainActivity,LoginFragment.this,id,password);
+                    userLoginCheck.checkUser();
                 } else {
                     Toast.makeText(getActivity(),"账号或密码不能为空",Toast.LENGTH_SHORT).show();
                 }
+                clearFocus();
+                passwordView.setText("");
             }
         });
+    }
+
+    /**
+     * 服务器响应后的处理
+     * @param msg 服务器返回信息，登录失败会返回errorMessage，否则为用户信息
+     */
+    public void dealLoginResult(JSONObject msg){
+        String errorMessage = null;
+        try {
+            errorMessage = msg.getString("errorMessage");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        // 判断是否返回errorMessage
+        if(errorMessage == null){
+            SharedPreferences sharedPreferences = mainActivity.getSharedPreferences("user", Context.MODE_PRIVATE);
+            Map<String,String> map = (Map<String, String>) sharedPreferences.getAll();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            // 判断sharedPreferences里有没有此用户信息
+            if(map.containsKey(id)){
+                String value = map.get(id);
+                try {
+                    JSONObject jsonObject = new JSONObject(value);
+                    // 修改为已登录状态，此状态只允许一个账号为true
+                    jsonObject.put("isLogin",true);
+                    editor.putString(id,String.valueOf(jsonObject));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    // 不存在则新建添加
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.put("id",String.valueOf(msg.getInt("userId")));
+                    jsonObject.put("password",msg.getString("userPasswd"));
+                    jsonObject.put("username",msg.getString("userName"));
+                    jsonObject.put("isLogin",true);
+                    editor.putString(id,String.valueOf(jsonObject));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            editor.apply();
+            Toast.makeText(getActivity(),"登录成功",Toast.LENGTH_SHORT).show();
+            // 传用户信息到账户界面进行展示
+            Bundle bundle = new Bundle();
+            bundle.putString("msg",String.valueOf(msg));
+            myAccountFragment.setArguments(bundle);
+            backView.performClick();
+        } else {
+            Log.d("ERRORMESSAGE",errorMessage);
+            Toast.makeText(getActivity(),errorMessage,Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**
@@ -126,11 +167,16 @@ public class LoginFragment extends Fragment {
     @Override
     public void onHiddenChanged(boolean hidden) {
         if(!hidden){
-            usernameView.setText("");
+            idView.setText("");
             passwordView.setText("");
         }
+    }
+
+    private void clearFocus(){
+        idView.clearFocus();
+        passwordView.clearFocus();
         // 隐藏键盘
-        InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(view.getWindowToken(),0);
     }
 }
