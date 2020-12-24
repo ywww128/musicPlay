be940512cd88bbba42a64b9d2979899df2869aed
ywww <1285324136@qq.com>
2020-12-24 21:39:49 +0800

    注册连接服务器实现

--- app/src/main/java/com/example/musicplayer/fragment/RegisterFragment.java
+++ app/src/main/java/com/example/musicplayer/fragment/RegisterFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,6 +17,7 @@ import androidx.fragment.app.Fragment;
 
 import com.example.musicplayer.R;
 import com.example.musicplayer.activity.MainActivity;
+import com.example.musicplayer.volley.UserRegister;
 import com.xuexiang.xui.widget.button.roundbutton.RoundButton;
 
 import org.json.JSONException;
@@ -31,6 +33,7 @@ import java.util.Set;
  */
 public class RegisterFragment extends Fragment {
     private View view;
+    private EditText idView;
     private EditText usernameView;
     private EditText passwordView;
     private EditText passwordConfirmView;
@@ -41,6 +44,7 @@ public class RegisterFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_register,null);
+        idView = view.findViewById(R.id.id_register_text);
         usernameView = view.findViewById(R.id.username_register_text);
         passwordView = view.findViewById(R.id.password_register_text);
         passwordConfirmView = view.findViewById(R.id.confirm_password_register_text);
@@ -56,6 +60,7 @@ public class RegisterFragment extends Fragment {
         backView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                clearFocus();
                 mainActivity.getManager().popBackStack();
             }
         });
@@ -64,43 +69,58 @@ public class RegisterFragment extends Fragment {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                clearFocus();
+                String id = String.valueOf(idView.getText());
                 String username = String.valueOf(usernameView.getText());
                 String password = String.valueOf(passwordView.getText());
                 String confirm_password = String.valueOf(passwordConfirmView.getText());
-                JSONObject jsonObject = new JSONObject();
+                JSONObject msg = new JSONObject();
                 if(username != "" && password != ""){
                     // 验证两次输入的密码是否一致
                     if(!password.equals(confirm_password)){
                         Toast.makeText(getActivity(),"密码不一致",Toast.LENGTH_SHORT).show();
+                        passwordView.setText("");
+                        passwordConfirmView.setText("");
                         return;
                     }
-                    SharedPreferences sharedPreferences = mainActivity.getSharedPreferences("user",Context.MODE_PRIVATE);
-                    Map<String,String> map = (Map<String, String>) sharedPreferences.getAll();
-                    Set<String> set = map.keySet();
-                    // 遍历查看账号是否已经注册
-                    for(String k : set){
-                        if(k.equals(username)){
-                            Toast.makeText(getActivity(),"账号已存在",Toast.LENGTH_SHORT).show();
-                            return;
-                        }
-                    }
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
                     try {
-                        jsonObject.put("username",username);
-                        jsonObject.put("password",password);
-                        jsonObject.put("isLogin",false);
+                        msg.put("username",username);
+                        msg.put("passwd",password);
+                        msg.put("id",id);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                    editor.putString(username,String.valueOf(jsonObject));
-                    editor.commit();
-                    Toast.makeText(getActivity(),"注册成功",Toast.LENGTH_SHORT).show();
-                    mainActivity.getManager().popBackStack();
+                    // 进行服务器检验，如果符合要求即注册
+                    UserRegister userRegister = new UserRegister(mainActivity,RegisterFragment.this,msg);
+                    userRegister.register();
                 } else {
                     Toast.makeText(getActivity(),"账号密码不为空",Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
+    }
+
+    public void dealRegisterResult(int result){
+        if(result == 0){
+            Toast.makeText(getActivity(),"账号已存在",Toast.LENGTH_SHORT).show();
+            idView.setText("");
+        } else {
+            Toast.makeText(getActivity(),"注册成功",Toast.LENGTH_SHORT).show();
+            mainActivity.getManager().popBackStack();
+        }
+    }
+
+    /**
+     * 取消输入框焦点
+     */
+    private void clearFocus(){
+        usernameView.clearFocus();
+        idView.clearFocus();
+        passwordView.clearFocus();
+        passwordConfirmView.clearFocus();
+        // 隐藏键盘
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(),0);
     }
 }
---
+++ app/src/main/java/com/example/musicplayer/volley/UserRegister.java
@@ -0,0 +1,74 @@
+package com.example.musicplayer.volley;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import androidx.fragment.app.Fragment;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.musicplayer.fragment.RegisterFragment;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ywww
+ * @date 2020-12-22 0:04
+ */
+public class UserRegister extends Application {
+
+    private Context context;
+    private Fragment fragment;
+    private JSONObject msg;
+    private RequestQueue mQueue;
+    private String url = "http://10.0.2.2:9988/user/insert";
+    public UserRegister(Context context, Fragment fragment, JSONObject msg){
+        this.context = context;
+        this.fragment = fragment;
+        this.msg = msg;
+        Log.d("msg",String.valueOf(msg));
+        initRequestQueue();
+    }
+
+    private void initRequestQueue(){
+        this.mQueue = Volley.newRequestQueue(context);
+    }
+
+    public void register(){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,url,new StringListener(),new StringError()){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> map = new HashMap<>(100);
+                map.put("msg",String.valueOf(msg));
+                return map;
+            }
+        };
+        mQueue.add(stringRequest);
+    }
+
+    private class StringListener implements Response.Listener<String> {
+
+        @Override
+        public void onResponse(String response) {
+            RegisterFragment registerFragment = (RegisterFragment) fragment;
+            registerFragment.dealRegisterResult(Integer.parseInt(response));
+        }
+    }
+
+    private class StringError implements Response.ErrorListener{
+
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("TAG",error.toString());
+        }
+    }
+}
